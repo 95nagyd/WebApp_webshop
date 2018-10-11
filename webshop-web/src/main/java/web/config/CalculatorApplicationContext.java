5201eef373e5dc847c748cc7fe198b571b94ffdf
95.nagy.d@gmail.com <Rbableves9>
2018-10-11 08:50:50 +0200

    beans created in CalculatorApplicationContext.java class

--- webshop-web/src/main/java/web/config/CalculatorApplicationContext.java
+++ webshop-web/src/main/java/web/config/CalculatorApplicationContext.java
@@ -1,6 +1,25 @@
 package web.config;
+import hu.iit.me.CalculatorController;
+import hu.iit.me.service.BasicCalculator;
+import hu.iit.me.service.Calculator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class CalculatorApplicationContext {
 
+    @Bean
+    public CalculatorController calculatorController(){
+        return new CalculatorController(calculator());
+    }
 
+    @Bean
+    public Calculator calculator(){
+        return new BasicCalculator();
+    }
+
+    @Bean
+    public  String helloWorld(){
+        return "hello world!";
+    }
 }
---
+++ webshop-web/src/main/java/web/config/Runnable.java
@@ -0,0 +1,8 @@
+package web.config;
+
+public class Runnable {
+
+    public static void main(String[] args) {
+
+    }
+}
