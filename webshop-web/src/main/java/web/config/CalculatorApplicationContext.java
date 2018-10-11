a96c9a4bc836ff2c73ad49b8e7545787c5bdd971
95.nagy.d@gmail.com <Rbableves9>
2018-10-11 09:18:56 +0200

    CalculatorApplicationMethodContext.java and Runnable.java created

--- webshop-web/src/main/java/web/config/CalculatorApplicationContext.java
+++ webshop-web/src/main/java/web/config/CalculatorApplicationContext.java
@@ -1,25 +1,17 @@
 package web.config;
 import hu.iit.me.CalculatorController;
-import hu.iit.me.service.BasicCalculator;
 import hu.iit.me.service.Calculator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CalculatorApplicationContext {
 
-    @Bean
-    public CalculatorController calculatorController(){
-        return new CalculatorController(calculator());
-    }
+    @Autowired Calculator calculator;
 
     @Bean
-    public Calculator calculator(){
-        return new BasicCalculator();
-    }
-
-    @Bean
-    public  String helloWorld(){
-        return "hello world!";
+    public CalculatorController calculatorController() {
+        return new CalculatorController(calculator);
     }
 }
