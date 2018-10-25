7fdf8c58e0d6e56f58009714ed2f471e973ef5f8
95.nagy.d@gmail.com <Rbableves9>
2018-10-25 10:00:30 +0200

    empty CalculatorDAO.java and CalculatorDAOimpl.java has been made

---
+++ webshop-dao/src/main/java/hu/iit/me/dao/CalculatorDAOimpl.java
@@ -0,0 +1,6 @@
+package hu.iit.me.dao;
+
+public class CalculatorDAOimpl implements CalculatorDAO{
+
+    //TODO calculator DAO implementation
+}
---
+++ webshop-service/src/main/java/hu/iit/me/dao/CalculatorDAO.java
@@ -0,0 +1,7 @@
+package hu.iit.me.dao;
+
+public interface CalculatorDAO {
+
+    //TODO database services
+
+}
